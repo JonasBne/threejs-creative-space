5d1e4700741b2e97775304e76c65e9d118c93eb2
Jonas Boone <boone.jonas@outlook.be>
2023-01-16 20:03:41 +0100

    feat: added new threejs project based on starter template

---
+++ packages/portfolio-shapes/src/main.ts
@@ -0,0 +1,77 @@
+import './app/app.element.ts';
+import './styles.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// canvas
+const canvas = document.getElementsByClassName('webgl')[0] as HTMLCanvasElement;
+
+// initiate a scene
+const scene = new THREE.Scene();
+
+// mesh
+const geometry = new THREE.BoxGeometry(0.75, 0.75, 0.75);
+const material = new THREE.MeshBasicMaterial({ color: 'green'});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// resize handler
+window.addEventListener('resize', () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // update renderer and pixel ration
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// fullscreen handler
+window.addEventListener('dblclick', () => {
+  if (!document.fullscreenElement) {
+    return canvas.requestFullscreen();
+  }
+  return document.exitFullscreen();
+});
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000,
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+// add size to renderer to avoid pixelated view
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const animate = () => {
+  // enable damping
+  controls.update();
+  // render scene
+  renderer.render(scene, camera);
+  // pass reference to itself to create infinite loop of frames
+  window.requestAnimationFrame(animate);
+};
+
+animate();
